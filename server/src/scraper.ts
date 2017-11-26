3d2687246d6e94ca40599d5e0508dc9ac25dfca1
Erik Goldman <erik.goldman@gmail.com>
2017-11-25 16:55:57 -0800

    scraper push test

--- server/src/scraper.ts
+++ server/src/scraper.ts
@@ -1,19 +1,48 @@
 import * as express from "express";
 import * as puppeteer from "puppeteer";
 
+const scrapePage = (page: puppeteer.Page, pageNum: number) => {
+  (async () => {
+    await page.goto(`https://sonja2018.nationbuilder.com/admin/financial_transactions?page=${pageNum}`);
+
+    const buttonSelector = "a.btn-muted";
+    await page.waitFor(buttonSelector);
+
+    const links = await page.$$eval(buttonSelector, btn => btn.href);
+    console.log(links);
+  });
+}
+
 export const scrapeNationbuilder = (req: express.Request, res: express.Response) => {
   (async () => {
+    if (!process.env.NATIONBUILDER_EMAIL || !process.env.NATIONBUILDER_PASSWORD) {
+      throw new Error("you need to specify a nationbuilder user or password in env vars to use this");
+    }
+    const nbEmail = process.env.NATIONBUILDER_EMAIL as string;
+    const nbPassword = process.env.NATIONBUILDER_PASSWORD as string;
+
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
-    await page.goto("https://sonja2018.nationbuilder.com");
-    await page.screenshot().then(function (buffer) {
-      res.setHeader('Content-Disposition', 'attachment;filename=ss.png"');
-      res.setHeader('Content-Type', 'image/png');
-      res.send(buffer)
-    });
+    await page.goto("https://sonja2018.nationbuilder.com/forms/user_sessions/new");
+
+    // log in
+    const emailSelector = "#user_session_email";
+    const passwordSelector = "#user_session_password";
+
+    await page.waitFor(emailSelector);
+    await page.type(emailSelector, nbEmail);
+    await page.waitFor(passwordSelector);
+    await page.type(passwordSelector, nbPassword);
+
+    const loginSelector = "$('.submit-button[value=\"Sign in with email\"]')";
+    await page.waitFor(loginSelector);
+    await page.click(loginSelector);
+    await page.waitForSelector('$(".user_pic")');
+
+    await scrapePage(page, 1);
 
     await browser.close();
   })();
